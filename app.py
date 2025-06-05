deb7665cf31c1d95145aecbb9db30f57baaeee06
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-06-05 21:33:34 +0500

    better flow might be

--- app.py
+++ app.py
@@ -33,30 +33,29 @@ def upload_csv(file=None):
 
 # Create and run the MCP server
 if __name__ == "__main__":
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
-    service = build("sheets", "v4", credentials=creds)
-    sheet = service.spreadsheets()
-
     with gr.Blocks() as demo:
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+
+        service = build("sheets", "v4", credentials=creds)
+        sheet = service.spreadsheets()
         df = None
         with gr.Row():
             with gr.Column():
@@ -74,5 +73,4 @@ if __name__ == "__main__":
                 )
             with gr.Column():
                 gr.DataFrame(value=df, label="DataFrame")
-
     demo.launch(mcp_server=True)
